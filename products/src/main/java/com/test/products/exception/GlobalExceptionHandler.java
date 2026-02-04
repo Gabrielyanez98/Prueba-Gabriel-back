b92b969e286f5541e49cc30a65fc4a59c5e8ba42
gabriel <gabriyanezvallverdu@gmail.com>
2026-02-04 19:48:02 +0100

    feature: Tests performed locally and working

---
+++ products/src/main/java/com/test/products/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.test.products.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, Object>> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("timestamp", LocalDateTime.now());
+        errorResponse.put("status", ex.getStatusCode().value());
+        errorResponse.put("message", ex.getReason());
+        
+        return new ResponseEntity<>(errorResponse, ex.getStatusCode());
+    }
+}
